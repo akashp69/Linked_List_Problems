8852c118892647e1e5e62d6397d1a34132d5aa92
akashp69 <akashpakhare499@gmail.com>
2022-12-12 02:06:06 +0530

    Added: Program for delete 40 value  node in linkedlist

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 class LinkListMain {
     /**
-     *  This is Main method is used for  Ability to add 40 value element after the 30 value element in the LinkedList
+     *  This is Main method is used for  Ability to delete 40 value  element in the LinkedList
      */
     public static void main(String[] args) {
 
@@ -10,10 +10,15 @@ class LinkListMain {
         list1.add(56);
         list1.add(30);
         list1.add(70);
-        list1.show();
+
         list1.insertAtMid(40);
-        System.out.println("Add 40 after 30 in linked list");
+        System.out.println("List befor deletion");
         list1.show();
+        list1.popDeleteData(2);
+        System.out.println("List after deletion");
+        list1.show();
+
+        System.out.println("The size of list is : " + list1.size());
         System.out.println();
 
     }
