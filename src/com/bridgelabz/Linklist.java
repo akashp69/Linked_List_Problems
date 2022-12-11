78379b901f17a7df7c59aff988909809cf34296f
akashp69 <akashpakhare499@gmail.com>
2022-12-12 01:23:29 +0530

    Added: Program for inserting the element in between two elements

--- src/com/bridgelabz/Linklist.java
+++ src/com/bridgelabz/Linklist.java
@@ -4,18 +4,45 @@ public class Linklist<T> {
     Node<T> head;
     Node<T> tail;
     /**
-     * This method is used for append the elements in linked list
+     * This method is used for add the elements in linked list
      */
 
-    public void append(T data) {
+    public void add(T data) {
         Node<T> newNode = new Node(data);
         if (head == null) {
             head = newNode;
+            tail = newNode;
         } else {
             tail.next = newNode;
+            tail = newNode;
         }
-        tail = newNode;
     }
+    /**
+     * This method is used for add data between two elements
+     */
+    public void insertAtMid(T data) {
+        if (head == null) {
+            head = new Node(data);
+        } else {
+            Node newNode = new Node(data);
+            Node temp = head;
+            int length = 0;
+            while (temp != null) {
+                length++;
+                temp = temp.next;
+            }
+            int count = ((length % 2) == 0) ? (length / 2) : (length + 1) / 2;
+            temp = head;
+
+            while (count-- > 1) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+
+        }
+    }
+
 
     /**
      * This method is used for showing the elements in linked list
