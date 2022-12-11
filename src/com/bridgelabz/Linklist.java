26e04ef8eee6efc9163e1e7fc91f30294c779e1c
akashp69 <akashpakhare499@gmail.com>
2022-12-12 01:30:13 +0530

    Added: Program for delete the first element in linked list

--- src/com/bridgelabz/Linklist.java
+++ src/com/bridgelabz/Linklist.java
@@ -18,31 +18,15 @@ public class Linklist<T> {
         }
     }
     /**
-     * This method is used for add data between two elements
+     * This method is used for delete the elements in linked list
      */
-    public void insertAtMid(T data) {
-        if (head == null) {
-            head = new Node(data);
-        } else {
-            Node newNode = new Node(data);
-            Node temp = head;
-            int length = 0;
-            while (temp != null) {
-                length++;
-                temp = temp.next;
-            }
-            int count = ((length % 2) == 0) ? (length / 2) : (length + 1) / 2;
-            temp = head;
-
-            while (count-- > 1) {
-                temp = temp.next;
-            }
-            newNode.next = temp.next;
-            temp.next = newNode;
-
+    public void pop() {
+        if (this.head != null) {
+            Node temp = this.head;
+            this.head = this.head.next;
+            temp = null;
         }
     }
-
 
     /**
      * This method is used for showing the elements in linked list
